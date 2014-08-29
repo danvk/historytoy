f256d5aa904c96d96d976316a76f9232ada82991
Dan Vanderkam <danvdk@gmail.com>
2014-08-29 16:29:04 -0400

    reasonable set of events

--- history.js
+++ history.js
@@ -1,1 +1,21 @@
 // An attempt to build history into app.html!
+$(function() {
+
+  $(window)
+    .on('showGrid', function() {
+      console.log('showGrid');
+    })
+    .on('hideGrid', function() {
+      console.log('hideGrid');
+    })
+    .on('selectPanel', function(e, panelNum) {
+      console.log('selected panel', panelNum);
+    })
+    .on('collapsePanel', function(panelNum) {
+      console.log('collapsed panel');
+    })
+    .on('expandPanel', function(panelNum) {
+      console.log('expanded panel');
+    })
+
+});
